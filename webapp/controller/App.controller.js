3a9b0fa7bf3634a6e59b6a17db34295f5cf3e634
inigoesteban <nack_z4@hotmail.com>
2025-01-17 10:57:32 +0000

    step-17

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+], (Controller, MessageToast) => {
+   "use strict";
+
+   return Controller.extend("ui5.walkthrough.controller.App", {
+   });
+});
